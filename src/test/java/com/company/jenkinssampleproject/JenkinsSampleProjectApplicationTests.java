36b16c24c0cc2a0cd89064c0cf7040fee46f3a96
Mirislom11 <mirislomzoirov2003@gmail.com>
2024-01-31 06:36:46 +0500

    first commit

---
+++ src/test/java/com/company/jenkinssampleproject/JenkinsSampleProjectApplicationTests.java
@@ -0,0 +1,13 @@
+package com.company.jenkinssampleproject;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JenkinsSampleProjectApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
